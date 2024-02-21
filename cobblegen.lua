c6bc1333d9145aae05a8f275928ca5e05dd28326
LoonOnMoon <loononmoon1@gmail.com>
2024-02-21 21:25:53 +0100

    Mining doesn't use fuel :)

--- cobblegen.lua
+++ cobblegen.lua
@@ -1,34 +1,9 @@
-os.loadAPI("/git/turtleUtils.lua")
-
-local CanDrop = true
-
 local function Main()
     while true do
-        if turtle.getFuelLevel == 0 then
-            turtle.select(1)
-            if turtle.getItemCount() == 0 then
-                turtle.suckUp()
-                turtleUtils.Refuel()
-            end
 
-            turtle.select(2)
-        end
-
-        local DidDig = false
-        if CanDrop then
-            if turtle.dig() then
-                DidDig = true
-            end
-        end
-
-        if (not turtle.dropDown()) and DidDig then
+        if turtle.dig() and (not turtle.dropDown()) then
             sleep(60)
-            CanDrop = false
-        else
-            CanDrop = true
         end
-
-        print(turtle.getFuelLevel())
     end
 end
 
